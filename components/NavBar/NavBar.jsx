616efdd125f5953dc97553eb26c4f7810fc254ee
Anzar Usmani <anzarusmani11@gmail.com>
2024-01-18 21:49:42 +0530

    Update NavBar.jsx

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -23,11 +23,60 @@ const NavBar = () => {
     const [profile,setProfile]=useState(false);
     const [openSideMenu,setOpenSideMenu]=useState(false);
 
+    const openMenu=(e)=>{
+        const btnText=e.target.innerText;
+        if(btnText =="Discover"){
+            setDiscover(true);
+            setHelp(false);
+            setNotification(false);
+            setProfile(false);
+            }else if (btnText=="Help Center"){
+                setDiscover(false);
+                setHelp(true);
+                setNotification(false);
+                setProfile(false);
+        }
+        else{
+            setDiscover(false);
+            setHelp(false);
+            setNotification(false);
+            setProfile(false);
+        }
+    };
 
     return (
     <div className={Style.navbar}>
-        <div className={Style.navbar_contain}></div</</div>
-    )
+        <div className={Style.navbar_container}>
+        <div className={Style.navbar_container_left}>
+            <div className={Style.logo}>
+                <Image 
+                src={images.logo}
+                alt="NFT MarketPlace "
+                width={100}
+                height={100}
+                />
+            </div>
+            <div className={Style.navbar_container_left_box_input}>
+                <div className={Style.navbar_container_left_box_input_box}>
+                    <input type='text' placeholder="Search NFT"/>
+                    <BsSearch onClick={()=>{}} className={Style.search_con} />
+                </div>
+            </div>
+        </div>
+
+        //End OF LEFT Section
+        <div className={Style.navbar_container_right}>
+          <div className={Style.navbar_container_right_discover}>
+            <p onClick={(e)=>{}}>Discover</p>
+            <div className={Style.navbar_container_right_discover_box}>
+                <Discover/>
+            </div>
+            </div>  
+
+        </div>
+        </div>
+        </div>
+    );
 };
 
 export default NavBar;
