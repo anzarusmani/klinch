4f8f85d125da76c85e095f22bc6ce5cd668cef4d
vibrant001 <117892031+vibrant001@users.noreply.github.com>
2024-01-18 10:59:51 +0530

    25:29

---
+++ components/NavBar/NavBar.jsx
@@ -0,0 +1,22 @@
+import React, {useState, useEffect} from "react";
+import Image from "next/image";
+import Link from "next/link";
+//----Import Icon
+
+import {MdNotification} from "react-icons/md";
+import {BsSearch} from "react-icons/bs";
+import {CgMenuLeft, CgMenuRight} from "react-icons/cg";
+
+
+
+
+//INTERNAL IMPORT
+import Style from "./NavBar.module.css";
+import {Discover, HelpCenter, Notification, Profile, SideBar} from "./index";
+import {Button} from "../componentindex";
+const NavBar = () => {
+    return <div>Navigation Bar</div>;
+    
+};
+
+export default NavBar;
---
+++ components/NavBar/SideBar/SideBar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const SideBar = () => {
+    return <div>Side Bar</div>;
+    
+};
+
+export default SideBar;
