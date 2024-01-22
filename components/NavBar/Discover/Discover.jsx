009468ea473e58d6d0f693d3c065420692dbb1a7
Anzar Usmani <anzarusmani11@gmail.com>
2024-01-22 22:18:04 +0530

    52:02

--- components/NavBar/Discover/Discover.jsx
+++ components/NavBar/Discover/Discover.jsx
@@ -1,6 +1,28 @@
 import React from 'react';
+import Link from 'next/link';
+
+//Internal import
+import Style from './Discover.module.css';
 
 const Discover = () => {
+
+    // discover navigation menu
+    const discover =[
+    {
+        name:"Collection",
+        link:"collection"
+    },
+    {
+        name:"Search",
+        link:"search"
+    },
+    {
+        name:"Author Profile",
+        link:"author-profile"
+    },
+    
+]
+
     return <div>Discover</div>;
     
 };
