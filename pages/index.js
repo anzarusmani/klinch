8ef0e28f89514dc99e829f7949d9c3cc19bd4d05
Anzar Usmani <anzarusmani11@gmail.com>
2024-02-06 02:34:51 +0530

    13th vid, 10:57

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 //INTERNAL IMPORT
 import Style from '../styles/index.module.css'
-import { BigNFTSlider, HeroSection,Service, Subscribe, Title, Category, Filter } from '../components/componentsindex';
+import { BigNFTSlider, HeroSection,Service, Subscribe, Title, Category, Filter,NFTCard, } from '../components/componentsindex';
 
 const Home=()=>{
   return (
@@ -15,6 +15,7 @@ const Home=()=>{
         paragraph="Discover the Finest Selection of Featured NFTs on Klinch."
       />
       <Filter/>
+      <NFTCard/>
       <Title 
         heading="Browse by category" 
         paragraph="Explore the Diverse World of NFTs on Klinch."
