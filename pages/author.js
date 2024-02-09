663b329ef40f59c7520fd46cf12be7702f30ef6b
Anzar Usmani <anzarusmani11@gmail.com>
2024-02-10 03:36:07 +0530

    22th 5:47

---
+++ pages/author.js
@@ -0,0 +1,33 @@
+import React,{useState, useEffect} from 'react';
+
+//INTERNAL IMPORT
+import Style from "../styles/author.module.css";
+import {Banner} from "../collectionPage/collectionindex"; 
+import images from "../img";
+// import {AuthorProfileCard,AuthorTaps,TabCard} from
+
+const author = () => {
+    const popularArray=[
+        images.user1,
+        images.user2,
+        images.user3,
+        images.user4,
+        images.user5,
+        images.user6,
+        images.user7,
+        images.user8,
+    ]
+const [collectiables,setCollectiables]=useState(true);
+const [ created,setCreated]=useState(false);
+const [ like,setLike]=useState(false);
+const [ follower,setFollower]=useState(false);
+const [ following,setFollowing]=useState(false);
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default author
