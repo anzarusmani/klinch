056c966196d641978f0f6700311d8eb0dabe1613
Anzar Usmani <anzarusmani11@gmail.com>
2024-02-09 14:17:03 +0530

    time check(till earn img)

--- components/componentsindex.js
+++ components/componentsindex.js
@@ -14,6 +14,7 @@ import FollowerTab from "./FollowerTab/FollowerTab";
 import AudioLive from "./AudioLive/AudioLive";
 import LikeProfile from "./LikeProfile/LikeProfile";
 import Slider from './Slider/Slider';
+import Brand from './Brand/Brand';
 export {
     Button,
     Footer,
@@ -31,4 +32,5 @@ export {
     AudioLive,
     LikeProfile,
     Slider,
+    Brand,
 };
