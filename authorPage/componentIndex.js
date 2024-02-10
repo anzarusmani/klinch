b0155c719be8e543e8e7c5cab6b4c6eae48c0fff
vibrant001 <117892031+vibrant001@users.noreply.github.com>
2024-02-10 13:48:09 +0530

    22nd done

--- authorPage/componentIndex.js
+++ authorPage/componentIndex.js
@@ -1,6 +1,7 @@
 import AuthorProfileCard from "./AuthorProfileCard/AuthorProfileCard";
 import AuthorTaps from "./AuthorTaps/AuthorTaps";
+import AuthorNFTCardBox from "./AuthorNFTCardBox/AuthorNFTCardBox";
 
 
 
-export {AuthorProfileCard,AuthorTaps};
+export {AuthorProfileCard,AuthorTaps,AuthorNFTCardBox};
