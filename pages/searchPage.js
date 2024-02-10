fed847e3c58ba72de5d3974d6123d79d3cbdf5c0
Anzar Usmani <anzarusmani11@gmail.com>
2024-02-10 23:35:53 +0530

    Update searchPage.js

---
+++ pages/searchPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+//INTERNAL IMPORT 
+import Style from '../styles/searchPage.module.css';
+
+const searchPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default searchPage
